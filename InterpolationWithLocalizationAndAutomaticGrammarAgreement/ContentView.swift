cb906f1dddd2fd9a0bc62f176a373e4e24673aa8
Gene Bogdanovich <genebogdanovich@gmail.com>
2022-04-23 15:23:12 +0300

    Initial Commit

---
+++ InterpolationWithLocalizationAndAutomaticGrammarAgreement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InterpolationWithLocalizationAndAutomaticGrammarAgreement
+//
+//  Created by Gene Bogdanovich on 23.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InterpolationWithLocalizationAndAutomaticGrammarAgreement/InterpolationWithLocalizationAndAutomaticGrammarAgreementApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterpolationWithLocalizationAndAutomaticGrammarAgreementApp.swift
+//  InterpolationWithLocalizationAndAutomaticGrammarAgreement
+//
+//  Created by Gene Bogdanovich on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct InterpolationWithLocalizationAndAutomaticGrammarAgreementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
